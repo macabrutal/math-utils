a747de088e35e390d4a209bb901f70d2067a26c4
macabrutal <maca@brutalrocks.com>
2021-11-09 19:53:05 -0300

    multiplicacion

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//SUMA
 function Suma(a,b) {
     console.log(a+b)
     return a + b
@@ -5,12 +6,20 @@ function Suma(a,b) {
 
 Suma(13,5)
 
-
+//RESTA
 function Restar(a,b) {
     console.log(a-b)
     return a - b
 }
 
 Restar(13,5)
+
+//multiplicación
+function Multiplicacion(a,b) {
+    console.log(a*b)
+    return a * b
+}
+
+Multiplicacion(3,6)
 
 
